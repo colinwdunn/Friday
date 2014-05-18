6bd2421ae615df7be9f0229ae3abfa14ebcc7fb1
Timothy Lee <timothy1ee@yahoo.com>
2014-05-17 21:07:44 -0700

    Initial Commit

---
+++ Friday/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Friday
+//
+//  Created by Timothy Lee on 5/17/14.
+//  Copyright (c) 2014 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FridayTests/FridayTests.m
@@ -0,0 +1,34 @@
+//
+//  FridayTests.m
+//  FridayTests
+//
+//  Created by Timothy Lee on 5/17/14.
+//  Copyright (c) 2014 codepath. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FridayTests : XCTestCase
+
+@end
+
+@implementation FridayTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
